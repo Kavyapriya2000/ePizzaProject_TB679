a13004397183a80bc03219f5a613eb065677abd8
Kavya Priya K <kavyapriya0806@gmail.com>
2026-01-08 10:19:56 +0530

    Created UserController with basic registerUser and ValidateUsermethods

---
+++ ePizza.API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ePizza.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("register-user")]
+
+        public async Task<IActionResult> RegisterUser()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("validate-user")]
+        public async Task<IActionResult> ValidateUser()
+        {
+            return Ok();
+        }
+    }
+}
